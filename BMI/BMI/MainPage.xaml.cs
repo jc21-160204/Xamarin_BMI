1fdf79fe64df5bd35555a24097a3c715ef20cfbb
雄也 山口 <160204@jc-21.jp>
2018-11-26 12:29:59 +0900

    initial

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -7,11 +7,68 @@ using Xamarin.Forms;
 
 namespace BMI
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
+
+
+        public partial class MainPage : ContentPage
         {
-            InitializeComponent();
+            public MainPage()
+            {
+                InitializeComponent();
+                ButtonBMI.Clicked += ButtonBMIClickEvent;
+                ButtonBMI2.Clicked += ButtonClickEvent2;
+            }
+
+            private void ButtonBMIClickEvent(object sender, EventArgs e)
+            {
+
+                float height = float.Parse(EntryHeight.Text);
+                float weight = float.Parse(Entry.Text);
+                LabelResult.Text = "" + weight / (height * height);
+
+                if (height <= 50)
+                {
+                    LabelResult.Text = "" + weight / (height * height);
+                }
+                else
+                {
+                    height = height / 100;
+                    LabelResult.Text = "" + weight / (height * height);
+                }
+            }
+            private void ButtonClickEvent2(object sender, EventArgs e)
+            {
+                float height = float.Parse(EntryHeight.Text);
+                float weight = float.Parse(Entry.Text);
+
+                if (height <= 3)
+                {
+                    LabelResult.Text = "" + weight / (height * height);
+                    float BMI = weight / (height * height);
+                    Navigation.PushModalAsync(new NavigationPage(new Page1(BMI)));
+                }
+                else
+                {
+                    height = height / 100;
+                    LabelResult.Text = "" + weight / (height * height);
+                    float BMI = weight / (height * height);
+                    Navigation.PushModalAsync(new NavigationPage(new Page1(BMI)));
+                }
+            }
+            private void ButtonBmiEvent(object sender, EventArgs e)
+            {
+
+                float height = float.Parse(EntryHeight.Text);
+                float weight = float.Parse(Entry.Text);
+
+                if (height <= 50)
+                {
+                    LabelResult.Text = "" + weight / (height * height);
+                }
+                else
+                {
+                    height = height / 100;
+                    LabelResult.Text = "" + weight / (height * height);
+                }
+            }
         }
     }
-}
---
+++ BMI/BMI/Page1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BMI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        private float BMI;
+
+        public Page1()
+        {
+            InitializeComponent();
+            buttonBack.Clicked += buttonBackClicked;
+        }
+
+        public Page1(float BMI)
+        {
+            InitializeComponent();
+            this.BMI = BMI;
+            labelBMI.Text = "BMI=" + BMI;
+            buttonBack.Clicked += buttonBackClicked;
+        }
+
+
+        private void buttonBackClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
